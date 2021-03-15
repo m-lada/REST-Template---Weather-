c0c0ffe0ab2b2bf301a0dd751189ce2ca84281c2
m-lada <maciej.lada@gmail.com>
2021-03-15 21:49:12 +0100

    Finish work
    1. Start
    
    2. Resolve A1
    
    3. Corrections A1
    
    3.1. Correct v2
    
    3.2. Correct v3

--- src/main/java/pl/maciek/resttemplate/model/WeatherMapper.java
+++ src/main/java/pl/maciek/resttemplate/model/WeatherMapper.java
@@ -1,6 +1,7 @@
 package pl.maciek.resttemplate.model;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class WeatherMapper {
 
@@ -11,10 +12,14 @@ public class WeatherMapper {
 
         newWeather.setCity(weatherDto.getName());
 
-        newWeather.setTemperatureCurrent(weatherDto.getWeatherParams().getTemperatureCurrent().subtract(KELVIN_ZERO).setScale(1));
-        newWeather.setTemperatureFelt(weatherDto.getWeatherParams().getTemperatureCurrent().subtract(KELVIN_ZERO).setScale(1));
-        newWeather.setTemperatureMin(weatherDto.getWeatherParams().getTemperatureMin().subtract(KELVIN_ZERO));
-        newWeather.setTemperatureMax(weatherDto.getWeatherParams().getTemperatureMax().subtract(KELVIN_ZERO));
+        newWeather.setTemperatureCurrent(weatherDto.getWeatherParams().getTemperatureCurrent().subtract(KELVIN_ZERO)
+                .setScale(1, RoundingMode.HALF_UP));
+        newWeather.setTemperatureFelt(weatherDto.getWeatherParams().getTemperatureCurrent().subtract(KELVIN_ZERO)
+                .setScale(1, RoundingMode.HALF_UP));
+        newWeather.setTemperatureMin(weatherDto.getWeatherParams().getTemperatureMin().subtract(KELVIN_ZERO)
+                .setScale(1, RoundingMode.HALF_UP));
+        newWeather.setTemperatureMax(weatherDto.getWeatherParams().getTemperatureMax().subtract(KELVIN_ZERO)
+                .setScale(1, RoundingMode.HALF_UP));
         newWeather.setPressure(weatherDto.getWeatherParams().getPressure());
         newWeather.setHumidity(weatherDto.getWeatherParams().getHumidity());
 
